c2c5f059a3d505e4d563f6aee4511b2aa214383a
Abhinav <abhinavdhere@gmail.com>
2017-05-09 01:54:36 +0530

    post detail views added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import blogPost
 
@@ -6,3 +6,7 @@ from .models import blogPost
 def post_list(request):
 	posts = blogPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
 	return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+	post = get_object_or_404(blogPost, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
